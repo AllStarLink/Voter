031c23629ef6caea59be27808d86d4f03df7e8c3
jdixon <jdixon@b9360e03-a0fa-4602-804e-483810d782e8>
2013-05-01 03:39:03 +0000

    Fixed little boo boo

--- board-firmware/src/Voter.c
+++ board-firmware/src/Voter.c
@@ -229,7 +229,7 @@ ROM char gpsmsg1[] = "GPS Receiver Active, waiting for aquisition\n", gpsmsg2[] 
 	entnewval[] = "Enter New Value : ", newvalchanged[] = "Value Changed Successfully\n",saved[] = "Configuration Settings Written to EEPROM\n", 
 	newvalerror[] = "Invalid Entry, Value Not Changed\n", newvalnotchanged[] = "No Entry Made, Value Not Changed\n",
 	badmix[] = "  ERROR! Host not acknowledging non-GPS disciplined operation\n",hosttmomsg[] = "  ERROR! Host response timeout\n",
-	VERSION[] = "1.12 04/30/2013";
+	VERSION[] = "1.13 04/30/2013";
 
 typedef struct {
 	DWORD vtime_sec;
